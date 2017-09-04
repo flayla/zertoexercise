c5acf8c184d4011237265e81ed456ffe7fd3ee27
Rotem.Akerman <rotem.akerman@zerto.com>
2017-09-04 23:47:33 +0300

    [master] init
    Branch: [master]

---
+++ index.js
@@ -0,0 +1,40 @@
+var http = require('http');
+var formidable = require("formidable");
+var util = require('util');
+
+console.log('Started.');
+
+var server = http.createServer(function (req, res) {
+	if (req.method.toLowerCase() == 'get') {
+		console.log('Recieved GET');
+        displayForm(res);
+    }
+	else if (req.method.toLowerCase() == 'post') {
+		console.log('Processing POST');
+		processForm(req,res);
+		//displayForm(res);
+    }
+});
+
+server.listen(8080);
+
+function displayForm (res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write('<html><head><title>Exercise</title></head><body><form method="post" enctype="multipart/form-data">Source:<input type="text" id ="source" value="C:\\Source" name="source"><br>Target:<input type="text" id="target" value="C:\\Target" name=target><br><input type="submit" value="Start Listening" name="action"><br><input type="button" value="Stop Listening" name="action"></form></body></html>');
+	res.end();
+}
+
+function processForm(req, res) {
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields) {
+        res.writeHead(200, {
+            'content-type': 'text/plain'
+        });
+		res.write(fields.action);
+		res.write('\n\n');
+        res.end(util.inspect({
+            fields: fields,
+        }));
+    });
+}
